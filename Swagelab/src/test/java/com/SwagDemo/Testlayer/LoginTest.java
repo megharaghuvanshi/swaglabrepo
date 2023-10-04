450b6355b0b2550c2f52ff2147f5fb6841ff1a75
megharaghuvansh <meghrajvansh18@gmail.com>
2023-10-04 08:54:23 +0530

    Testcases for login

--- Swagelab/src/test/java/com/SwagDemo/Testlayer/LoginTest.java
+++ Swagelab/src/test/java/com/SwagDemo/Testlayer/LoginTest.java
@@ -1,5 +1,19 @@
 package com.SwagDemo.Testlayer;
 
-public class LoginTest {
+import org.testng.annotations.Test;
+
+import com.SwagDemo.pagelayer.LoginPage;
+import com.SwagDemo.testBase.TestBase;
+
+public class LoginTest extends TestBase {
+	@Test
+	public void logintestwithvaliinputdata() throws InterruptedException 
+	{
+		LoginPage login =new LoginPage();
+		login.enterusername("standard_user");
+		login.enterPassword("secret_sauce");
+		Thread.sleep(2000);
+		login.clickOnLoginButton();
+	}
 
 }
